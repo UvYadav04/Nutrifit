c8209d435cd8a87e85367efb759b0c9b8533e35e
UvYadav04 <dineshnirban01@gmail.com>
2024-07-07 15:24:54 +0530

    new commit

--- Frontend/src/Components/Register.jsx
+++ Frontend/src/Components/Register.jsx
@@ -11,6 +11,7 @@ export default function Login() {
     const [userdata, setuserdata] = useState({ username: "", password: "", name: "", email: "" })
     const navigate = useNavigate()
     const [show, setshow] = useState(true)
+    const [loading, setloading] = useState(false)
     // console.log(logindata);
     const handlelogin = (e) => {
         const name = e.target.name;
@@ -37,6 +38,7 @@ export default function Login() {
     }
 
     const handleloginsubmit = async (e) => {
+        setloading(true)
         e.preventDefault();
         const response = await fetch("https://nutrifitbackend.onrender.com/register/login", {
             method: 'POST',
@@ -45,7 +47,7 @@ export default function Login() {
             },
             body: JSON.stringify({ logindata })
         })
-
+        setloading(false)
         const json = await response.json()
 
         if (!json.success)
@@ -64,7 +66,7 @@ export default function Login() {
     }
     const handlesign = async (e) => {
         // console.log("data : ", userdata);
-
+        setloading(true)
         e.preventDefault();
         const response = await fetch("https://nutrifitbackend.onrender.com/register/signup", {
             method: 'POST',
@@ -73,7 +75,7 @@ export default function Login() {
             },
             body: JSON.stringify({ userdata }),
         })
-
+        setloading(false)
         const json = await response.json()
 
         if (!json.success)
@@ -89,49 +91,55 @@ export default function Login() {
     }
 
     return (
-        <div className="loginpage container-fluid m-0 text-center" style={{
-            minHeight: "100vh", maxHeight: "100vh", backgroundColor: "#a8a5a5"
-        }} >
+        <>
+            <div className={!loading ? "loginpage container-fluid m-0 text-center" : "d-none"} style={{
+                minHeight: "100vh", maxHeight: "100vh", backgroundColor: "#a8a5a5"
+            }} >
 
-            <div className="row register justify-content-around align-items-md-center align-items-start mx-auto bg bg-white" style={{
-                backgroundColor: "beige"
-            }}>
-                <div className="col-xl-4 col-5 d-lg-inline d-none p-0 ">
-                    <img src={covers} className='w-100 m-0 rounded-2 h-100' alt="" />
-                </div>
-                <div className={show ? "col-xl-4 col-lg-5 col-md-7 col-sm-7 col-10 login  d-flex flex-column justify-content-center h-100 text-center show " : "d-none"}>
-                    <img src={usericon} className='mx-auto w-25 rounded-5' alt="" />
-                    <h4 className='fs-sm-1 fs-3'>Welcome back!</h4>
-                    <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text" name='username' placeholder='username' value={logindata.username} onChange={(e) => handlelogin(e)} />
-                    <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="password" name='password' placeholder='password' value={logindata.password} onChange={(e) => handlelogin(e)} />
-                    <div
-                        className="d-flex justify-content-between align-items-center mb-lg-4 mb-1 "
-                    >
-                        <div>
-                            <a href="/" onClick={(e) => handlenewuser(e)} ref={ref} className=" text-decoration-none text-primary fs-6">New user</a>
-                        </div>
-                        <div>
-                            <a href="/" className=" text-decoration-none text-primary fs-6">Forgot password?</a>
-                        </div>
+                <div className="row register justify-content-around align-items-md-center align-items-start mx-auto bg bg-white" style={{
+                    backgroundColor: "beige"
+                }}>
+                    <div className="col-xl-4 col-5 d-lg-inline d-none p-0 ">
+                        <img src={covers} className='w-100 m-0 rounded-2 h-100' alt="" />
                     </div>
-                    <button className='w-50 mx-auto rounded-5 bg bg-primary text-white fs-sm-4 fs-5' onClick={(e) => handleloginsubmit(e)}>Login</button>
-                </div>
-                <div className={show ? "d-none" : "col-xl-4 col-lg-5 col-md-7 col-sm-7 col-10 signup  d-flex flex-column show text-center justify-content-center h-100"} >
-                    <h1 className='fs-sm-1 fs-3'>Signup</h1>
-                    <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text" name='name' placeholder='Full Name' value={userdata.name} onChange={(e) => handlesignup(e)} />
-                    <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text " name='username' placeholder='username' value={userdata.username} onChange={(e) => handlesignup(e)} />
-                    <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text" name='email' placeholder='email' value={userdata.email} onChange={(e) => handlesignup(e)} />
-                    <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="password" name='password' placeholder='password' value={userdata.password} onChange={(e) => handlesignup(e)} />
-                    <div
-                        className="d-flex justify-content-between align-items-center mb-lg-2 mb-1"
-                    >
-                        <a href="#!" onClick={(e) => handlealreadyuser(e)} ref={ref} className=" text-decoration-none text-primary mb-0  fs-6">already have an account?</a>
+                    <div className={show ? "col-xl-4 col-lg-5 col-md-7 col-sm-7 col-10 login  d-flex flex-column justify-content-center h-100 text-center show " : "d-none"}>
+                        <img src={usericon} className='mx-auto w-25 rounded-5' alt="" />
+                        <h4 className='fs-sm-1 fs-3'>Welcome back!</h4>
+                        <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text" name='username' placeholder='username' value={logindata.username} onChange={(e) => handlelogin(e)} />
+                        <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="password" name='password' placeholder='password' value={logindata.password} onChange={(e) => handlelogin(e)} />
+                        <div
+                            className="d-flex justify-content-between align-items-center mb-lg-4 mb-1 "
+                        >
+                            <div>
+                                <a href="/" onClick={(e) => handlenewuser(e)} ref={ref} className=" text-decoration-none text-primary fs-6">New user</a>
+                            </div>
+                            <div>
+                                <a href="/" className=" text-decoration-none text-primary fs-6">Forgot password?</a>
+                            </div>
+                        </div>
+                        <button className='w-50 mx-auto rounded-5 bg bg-primary text-white fs-sm-4 fs-5' onClick={(e) => handleloginsubmit(e)}>Login</button>
                     </div>
-                    <button className='w-50 mx-auto rounded-5 bg bg-primary text-white fs-sm-4 fs-6 mt-4' onClick={(e) => handlesign(e)}>Sign up</button>
-                </div>
+                    <div className={show ? "d-none" : "col-xl-4 col-lg-5 col-md-7 col-sm-7 col-10 signup  d-flex flex-column show text-center justify-content-center h-100"} >
+                        <h1 className='fs-sm-1 fs-3'>Signup</h1>
+                        <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text" name='name' placeholder='Full Name' value={userdata.name} onChange={(e) => handlesignup(e)} />
+                        <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text " name='username' placeholder='username' value={userdata.username} onChange={(e) => handlesignup(e)} />
+                        <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="text" name='email' placeholder='email' value={userdata.email} onChange={(e) => handlesignup(e)} />
+                        <input className='my-md-3 my-sm-2 my-2 fs-sm-5 fs-6' type="password" name='password' placeholder='password' value={userdata.password} onChange={(e) => handlesignup(e)} />
+                        <div
+                            className="d-flex justify-content-between align-items-center mb-lg-2 mb-1"
+                        >
+                            <a href="#!" onClick={(e) => handlealreadyuser(e)} ref={ref} className=" text-decoration-none text-primary mb-0  fs-6">already have an account?</a>
+                        </div>
+                        <button className='w-50 mx-auto rounded-5 bg bg-primary text-white fs-sm-4 fs-6 mt-4' onClick={(e) => handlesign(e)}>Sign up</button>
+                    </div>
 
-            </div>
-        </div >
+                </div>
+            </div >
+
+            <div className={loading ? "loader d-flex justify-content-center align-items-center" : "d-none"}>
+                <h4>Loading.....</h4>
+            </div >
+        </>
     )
 
 }
